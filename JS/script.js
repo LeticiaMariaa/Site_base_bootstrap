67a30d88b2e91e67eec54f0d73b9796c689b0d14
LeticiaMariaa <79269905+LeticiaMariaa@users.noreply.github.com>
2021-10-14 12:26:14 -0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,14 @@
-alert("teste");
+function trocarcor(){
+    const texto = document.getElementById("Quem sou eu?");
+    texto.style.color = "blue";
+}
+
+function suma(){
+const texto = document.getElementById("Quem sou eu?");
+texto.style.display = "none";
+}
+
+function apareca(){
+    const texto = document.getElementById("Quem sou eu?");
+    texto.style.display = "block";
+}
